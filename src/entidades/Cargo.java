fa1442756c09eec356f9ac1ce5b3c7dac50f2361
saracbza <sasa.cb2002@gmail.com>
2023-10-20 15:57:47 -0300

    Criando a entidade Cargo

---
+++ src/entidades/Cargo.java
@@ -0,0 +1,26 @@
+package entidades;
+
+public class Cargo {
+    private int codigo;
+	private String nome;
+	private float valor_hr;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public float getValor_hr() {
+		return valor_hr;
+	}
+	public void setValor_hr(float valor_hr) {
+		this.valor_hr = valor_hr;
+	}
+}
