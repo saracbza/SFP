5a1706fbb6ba3811056b9bb0be1f5f500cd8361a
saracbza <sasa.cb2002@gmail.com>
2023-11-08 10:38:15 -0300

    Criando o método para salvar um cargo no banco

--- src/banco/CargoDAO.java
+++ src/banco/CargoDAO.java
@@ -9,6 +9,7 @@ import entidades.Cargo;
 public class CargoDAO {
 
 		private BD bd;
+		private String men, sql;
 
 		public CargoDAO() {
 			bd = new BD();
@@ -40,4 +41,29 @@ public class CargoDAO {
 				}
 				return lista;
 			}
+
+		/**
+		 * Realiza a gravacao de um cargo no banco de dados
+		 * @param c - o cargo a ser gravado
+		 * @return - mensagem de aviso do processo
+		 */
+		public String salvar(Cargo c) {
+			sql = "INSERT INTO cargo VALUES (?, ?)"; //nome e valor por hora
+			try {
+				bd.getConnection();
+				bd.st = bd.con.prepareStatement(sql);
+				bd.st.setString(1, c.getNome());
+				bd.st.setFloat(2, c.getValor_hr());
+				bd.st.executeUpdate();
+				
+				men = "Cargo "+c.getNome()+" inserido com sucesso!";
+			}
+			catch(SQLException erro) {
+				men = "Falha: verifique se o cargo já existe... " + erro;
+			}
+			finally {
+				bd.close();
+			}
+			return men;
+		}
 	}
