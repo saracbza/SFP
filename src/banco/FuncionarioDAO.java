f5c0850f88ed548503fb147fa97d55bc02891bd2
saracbza <sasa.cb2002@gmail.com>
2023-11-08 10:47:30 -0300

    Ajustando método de listagem de funcionarios

--- src/banco/FuncionarioDAO.java
+++ src/banco/FuncionarioDAO.java
@@ -1,37 +1,82 @@
 package banco;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import entidades.Cargo;
 import entidades.Funcionario;
 import entidades.Senioridade;
 
 public class FuncionarioDAO {
 	private BD bd;
+	private String men, sql;
+	
+	float valor_hr, porcentagem_rec;
+	int dependentes;
+	String sen, cargo;
+
+	/**
+	 * Retorna a senioridade do funcionario para futuro cadastro
+	 * @return - senioridade
+	 */
+	public String senioridade () {
+		return sen;
+	}
+	
+	/**
+	 * Retorna o cargo do funcionario para futuro cadastro
+	 * @return - cargo
+	 */
+	public String cargo () {
+		return cargo;
+	}
+	
+	/**
+	 * Retorna quantidade de dependentes do funcionario para futuro calculo do salario
+	 * @return - quantidade de dependentes
+	 */
+	public int dependentes () {
+		return dependentes;
+	}
+	
+	/**
+	 * Retorna valor que funcionario recebe por hora para futuro calculo do salario
+	 * @return - valor por hora que o funcionario recebe de acordo com seu cargo
+	 */
+	public float valor_hr () {
+		return valor_hr;
+	}
+	
+	/**
+	 * Retorna porcentagem de reconhecimento (convertida para decimal) do funcionario para futuro calculo do salario
+	 * @return - porcentagem de reconhecimento do funcionario de acordo com sua senioridade
+	 */
+	public float porcentagem_rec () {
+		porcentagem_rec /= 100;
+		return porcentagem_rec;
+	}
 	
 	public FuncionarioDAO() {
 		bd = new BD();
 	}
 	
 	/**
-	 * Lista todos os funcionarios cadastrados no sistema
+	 * Retorna todos os funcionarios cadastrados no banco
+	 * @param sql - comando para banco
+	 * @return - lista com todas os funcionarios
 	 */
-	public void listarTudo() {
-		if(bd.getConnection()) {
-			String sql = "SELECT * FROM funcionario";
+	public List<Funcionario> listarTudo(String sql) {
+		List<Funcionario> lista = new ArrayList<Funcionario>();
+		bd.getConnection();
 			try {
 				bd.st = bd.con.prepareStatement(sql);
 				bd.rs = bd.st.executeQuery();
 				
 				while(bd.rs.next()) { 
-					Funcionario f = new Funcionario();
-					f.setCodigo(bd.rs.getInt(1));
-					f.setNome(bd.rs.getString(2));
-					f.setCpf(bd.rs.getString(3));
-					f.setData_nasc(bd.rs.getDate(4));
-					f.setData_contrat(bd.rs.getDate(5));
-					f.setDependentes(bd.rs.getInt(6));
-					
-					System.out.println(f);
+					Funcionario f = new Funcionario(bd.rs.getInt(1), bd.rs.getString(2), bd.rs.getString(3),
+							bd.rs.getDate(4), bd.rs.getDate(5), bd.rs.getInt(6));
+					lista.add(f);
 				}
 			}
 			catch(SQLException e) {
@@ -40,12 +85,8 @@ public class FuncionarioDAO {
 			finally {
 				bd.close();
 			}
+			return lista;
 		}
-		
-		else {
-			System.out.println("Falha na conexão!");
-		}
-	}
 	
     /**
      * Lista nome, cargo e senioridade dos funcionarios cadastrados
